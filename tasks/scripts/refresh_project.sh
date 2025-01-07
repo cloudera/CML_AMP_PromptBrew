b6abd6f08f91fd2f492d5520e3890a506507c9ef
Elijah Williams <ewilliams@cloudera.com>
2025-01-07 10:40:10 -0700

    install node

--- tasks/scripts/refresh_project.sh
+++ tasks/scripts/refresh_project.sh
@@ -6,8 +6,20 @@ git pull --rebase
 
 ## Install dependencies and build ##
 cd FeApp
+
+set +e
+source scripts/load_nvm.sh > /dev/null
+nvm use 22
+return_code=$?
+set -e
+if [ $return_code -ne 0 ]; then
+    echo "NVM or required Node version not found.  Installing and using..."
+    bash scripts/install_node.sh
+
+    nvm use 22
+fi
+
 npm install @pnpm/exe
 ./node_modules/\@pnpm/exe/pnpm install
 ./node_modules/\@pnpm/exe/pnpm build
-
 
